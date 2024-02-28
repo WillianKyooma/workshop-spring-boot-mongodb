79ffb72b191fddfedd7ade88436394fba575f320
WillianKyooma <williancoden@gmail.com>
2024-02-28 02:38:59 -0300

    feat: Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -3,9 +3,17 @@ package com.nelioalves.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+@Id
 private String id;
 private String name;
 private String email;
@@ -49,18 +57,15 @@ public void setName(String name) {
 
 
 
-
 public String getEmail() {
 	return email;
 }
 
 
 
-
 public void setEmail(String email) {
 	this.email = email;
 }
-
 
 
 
--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-
-	
+@Autowired
+private UserService service;	
 	
 @GetMapping
 public ResponseEntity <List<User>>FindAll(){
 
-	User maria = new User("1", "Maria Brow","maria@gmail.com");
-	User alex = new User("2", "Alex Green","alex@gmail.com");
-	
-	List<User> list = new ArrayList<>();
-	list.addAll(Arrays.asList(maria,alex));
+	List<User> list = service.findAll();
 	return ResponseEntity.ok().body(list);
 	}
 }
