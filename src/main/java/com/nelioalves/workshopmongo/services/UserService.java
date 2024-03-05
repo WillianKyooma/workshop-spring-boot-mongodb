c251f82c26fdc37b497da9401671e862ef78844f
WillianKyooma <williancoden@gmail.com>
2024-03-05 11:52:47 -0300

    feature: delecao de usuario com delete

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.nelioalves.workshopmongo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
@@ -34,6 +35,14 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+		
+	}
+	
+	
 	
 	public User FromDTO(UserDTO objDto) {
 		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
