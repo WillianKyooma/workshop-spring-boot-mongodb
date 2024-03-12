ae85f2b0eeb2b2d8cb0c071b01529756b36e0d05
WillianKyooma <williancoden@gmail.com>
2024-03-12 01:46:56 -0300

    feature: projecao dos dados do autor com DTO

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -3,7 +3,7 @@ package com.nelioalves.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
-
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,14 +16,14 @@ public String id;
 private Date date;
 private String title;
 private String body;
-private User author;
+private  AuthorDTO author;
 
 public Post() {
 	
 }
 
 
-public Post(String id, Date date, String title, String body, User author) {
+public Post(String id, Date date, String title, String body, AuthorDTO author) {
 	super();
 	this.id = id;
 	this.date = date;
@@ -72,12 +72,12 @@ public void setBody(String body) {
 	this.body = body;
 }
 
-public User getAuthor() {
+public AuthorDTO getAuthor() {
 	return author;
 }
 
 
-public void setAuthor(User author) {
+public void setAuthor(AuthorDTO author) {
 	this.author = author;
 }
 
