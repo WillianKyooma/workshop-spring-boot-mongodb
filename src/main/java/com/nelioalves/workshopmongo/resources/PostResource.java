63c695ce5ba3d53f9e90069ee25156b727c55240
WillianKyooma <williancoden@gmail.com>
2024-03-14 00:10:10 -0300

    Correcao obtendo posts por id

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -1,30 +1,14 @@
 package com.nelioalves.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
-import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.PostService;
-import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -33,15 +17,9 @@ public class PostResource {
 @Autowired
 private PostService service;	
 	
-@GetMapping
-public ResponseEntity <List<UserDTO>>FindAll(){
-
-	List<User> list = service.findAll();
-	List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-	return ResponseEntity.ok().body(listDto);
-	}
 
 
+	
 @GetMapping(value ="/{id}")
 public ResponseEntity <Post>FindById(@PathVariable String id){
     
--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,17 +1,12 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
-import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
-import com.nelioalves.workshopmongo.repository.UserRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -20,16 +15,11 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 	
-	public List<User> findAll(){
+	public List<Post> findAll(){
 	return repo.findAll();	
 	}
 	
 	
-	/**
-	 * Retorna o usuário com o ID fornecido.
-	 * Utiliza findById que retorna um Optional<User> devido à remoção do método findOne do Spring Data MongoDB.
-	 * Lança ObjectNotFoundException se o usuário não for encontrado.
-	 */
 	public Post findById(String id) {
 	    return repo.findById(id)
 	               .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
